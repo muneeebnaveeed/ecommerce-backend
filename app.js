6597b7e6762ccf46a0dce9c6742556ebfb29f283
Muneeb Naveed <personal.muneebnaveed@gmail.com>
2020-11-24 15:22:14 +0500

    FEATURE: added cors fix

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const parser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const routes = require("./routes");
 const { auth } = require("./utils");
@@ -15,30 +16,31 @@ const { validateToken } = auth;
 
 // db connection
 mongoose
-  .connect(process.env.DB_CONNECTION || null, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("MongoDB Connected");
+    .connect(process.env.DB_CONNECTION || null, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("MongoDB Connected");
 
-    // init express server
-    const port = process.env.PORT || 3000;
-    app.listen(port, () => console.log(`Running server on Port ${port}`));
+        // init express server
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => console.log(`Running server on Port ${port}`));
 
-    // middlewares
-    app.use(parser.json());
+        // middlewares
+        app.use(parser.json());
+        app.use(cors());
 
-    // routes
-    app.use("/tags", validateToken, routes.tags);
-    app.use("/products", validateToken, routes.products);
-    app.use("/collections", validateToken, routes._collections);
-    app.use("/types", validateToken, routes.types);
-    app.use("/analytics", validateToken, routes.analytics);
-    app.use("/orders", validateToken, routes.orders);
-    app.use("/faqs", validateToken, routes.faqs);
+        // routes
+        app.use("/tags", routes.tags);
+        app.use("/products", routes.products);
+        app.use("/collections", routes._collections);
+        app.use("/types", routes.types);
+        app.use("/analytics", routes.analytics);
+        app.use("/orders", routes.orders);
+        app.use("/faqs", routes.faqs);
 
-    // auth
-    app.use("/auth", routes.auth);
-  })
-  .catch((err) => console.log(err));
+        // auth
+        app.use("/auth", routes.auth);
+    })
+    .catch((err) => console.log(err));
